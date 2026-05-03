4732ca8a944c1109a41bf0d926ca64dc75758532
GabrielRSSantos <gabrielsantos.new@gmail.com>
2026-05-03 15:52:58 -0300

    HurtBox added

--- Scripts/PlayerStates/hurt_state.gd
+++ Scripts/PlayerStates/hurt_state.gd
@@ -1,12 +1,24 @@
 extends PlayerState
 
+@export var knockback_horizontal: float = 50.0
+@export var knockback_vertical: float = -100.0
+
 func enter(previous_state_path: String, data := {}) -> void:
 	player.label_state.text = "HURT"
 	print("HURT")
+	var face_direction: float = -1.0 if player.animation_player.flip_h else 1.0
+
+	player.velocity.x = -face_direction * knockback_horizontal
+	player.velocity.y = knockback_vertical
+	
 	player.animation_player.play("Hurt")
+	await player.animation_player.animation_finished
+	
+	player.velocity.x = 0
+	finished.emit("Idle")
 
 func physics_update(_delta: float) -> void:
-	await player.animation_player.finished
-	finished.emit("Idle")
+	if not player.is_on_floor():
+		player.velocity += player.get_gravity() * _delta
 	
 	player.move_and_slide()
