fb9fe52eb7e6017ec609d59beb849bd893cd7feb
GabrielRSSantos <gabrielsantos.new@gmail.com>
2026-05-13 17:00:02 -0300

    Settings

---
+++ Scripts/GlobalScripts/global_settings.gd
@@ -0,0 +1,9 @@
+extends Node
+
+enum Language { POR, ENG }
+
+var volume = 50
+var language = Language.POR
+
+func apply_new_settings() -> void:
+	
---
+++ Scripts/WorldsScripts/world01.gd
@@ -0,0 +1,10 @@
+extends Node2D
+
+@onready var world_audio: AudioStreamPlayer = $WorldAudio
+
+func _ready() -> void:
+	world_audio
+
+
+func _process(delta: float) -> void:
+	pass
